3aaf0f6fdc0bc44606e8344e28fb2d0ee245b34f
James Ward <james@jamesward.org>
2013-10-19 10:30:57 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+WebJar for ui-select2
+
+More info: http://webjars.org
+
+Upstream: https://github.com/angular-ui/ui-select2
---
+++ src/main/resources/webjars-requirejs.js
@@ -0,0 +1,7 @@
+/*global requirejs */
+
+requirejs.config({
+  shim: {
+    'ui-select2': [ 'webjars!jquery.js', 'webjars!select2.js', 'webjars!angular.js' ]
+  }
+});
